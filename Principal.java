aef197632d3f4c24fc1f06b748da35631a889d3d
Manuel Cerezo <73239400+ManuelCerezo@users.noreply.github.com>
2020-12-26 12:23:05 +0100

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,17 +1,23 @@
+import java.util.ArrayList;
 
 public class Principal {
+	public static ArrayList<Lugar> lugares = new ArrayList<Lugar>();
+	public static ArrayList<Personaje> personajes = new ArrayList<Personaje>();
+	public static ArrayList<Objeto> objetos = new ArrayList<Objeto>();
 	
     public static void main(String args[]){ //Funcion para la lectura del fichero donde se interpretan las acciones
     	
     	leerFichero();
-        
-
+    	
     }
-    
     //Funciones
     public static void leerFichero() {
     	Fichero fichero = new Fichero();
     	fichero.inicializar();
+    	fichero.objetivos();
+    	fichero.imprimir();
     }
-    
+    public static void rondas() {
+    	
+    }
 }
