000f5433f203d93197d1d705d0f9b71a89bbb9fc
Manuel Cerezo <73239400+ManuelCerezo@users.noreply.github.com>
2020-12-26 12:22:35 +0100

    Update Personaje.java

--- Personaje.java
+++ Personaje.java
@@ -2,16 +2,46 @@
 public class Personaje {
 	public String nombre;
 	public String localizacion;
+	public String ObjetivoO="";
+	public String ObjetivoL="";
+	
 	
 	public Personaje(String nombre, String localizacion) {
+		super();
 		this.nombre = nombre;
 		this.localizacion = localizacion;
 	}
 	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getLocalizacion() {
+		return localizacion;
+	}
+	public void setLocalizacion(String localizacion) {
+		this.localizacion = localizacion;
+	}
+	public String getObjetivoO() {
+		return ObjetivoO;
+	}
+	public void setObjetivoO(String objetivoO) {
+		this.ObjetivoO = objetivoO;
+	}
+	public String getObjetivoL() {
+		return ObjetivoL;
+	}
+	public void setObjetivoL(String objetivoL) {
+		this.ObjetivoL = objetivoL;
+	}
 	@Override
 	public String toString() {
-		return "Personajes [nombre: " + nombre + ", localizacion: " + localizacion + "  ]";
+		return "Personaje [nombre=" + nombre + ", localizacion=" + localizacion + ", ObjetivoO=" + ObjetivoO
+				+ ", ObjetivoL=" + ObjetivoL + "]";
 	}
+
 	
 	
 }
