3854373cd7434e2704f91f27b036f4e668bc6500
Miguel <73239423+MiguelHGDC@users.noreply.github.com>
2020-12-26 16:55:52 +0100

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -8,6 +8,7 @@ public class Principal {
     public static void main(String args[]){ //Funcion para la lectura del fichero donde se interpretan las acciones
     	
     	leerFichero();
+    	rondas();
     	
     }
     //Funciones
@@ -17,7 +18,45 @@ public class Principal {
     	fichero.objetivos();
     	fichero.imprimir();
     }
-    public static void rondas() {
+    //1. Comprobacion de objetivos generales.
+    //2. Comprobacion de Localizaciones
+    //3. Ejecucion en caso de que no se haya cumplido.
+    
+    public static void rondas(){
+    	
+    	comprobaciones();
+    	
+    }
+    public static void comprobaciones(){ //HACER DOBLE FOR EACH
+    	int i = 0;
+    	int tablaflagsPL[] = new int[personajes.size()];
+    	int tablaflagsPO[] = new int[personajes.size()];
+    	
+    	for(Personaje pepino:personajes){
+    		if(pepino.getLocalizacion().equals(pepino.getObjetivoL())) {
+				tablaflagsPL[i]= 1;
+			}
+			else {
+				tablaflagsPL[i]= 0;
+			}
+    		for(Objeto melocoton:objetos){
+    			if(pepino.getNombre().equals(melocoton.getLocalizacion())){
+    				tablaflagsPO[i]= 1;
+    			}
+    			else {
+    				tablaflagsPO[i]= 0;
+    			}
+    		}
+    	}
+    	for(int coco:tablaflagsPL){
+    		System.out.println("Tabla de flags PL: "+coco);
+			}
+    	
+    	System.out.println("--------------------");
+    	
+    	for(int coco:tablaflagsPO){
+    		System.out.println("Tabla de flags PO: "+coco);
+			}
     	
     }
 }
