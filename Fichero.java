23d5367cad1b69febeef20001e3f755ab9138ade
Miguel <73239423+MiguelHGDC@users.noreply.github.com>
2020-12-27 13:35:11 +0100

    Update Fichero.java

--- Fichero.java
+++ Fichero.java
@@ -1,50 +1,169 @@
+import java.io.FileInputStream;
+import java.util.Scanner;
 
-public class Personaje {
-	public String nombre;
-	public String localizacion;
-	public String ObjetivoO="";
-	public String ObjetivoL="";
-	int IDPer; //numero de identificacion del personaje
+public class Fichero extends Principal{
 	
-	public Personaje(String nombre, String localizacion, int IDPer) {
-		super();
-		this.nombre = nombre;
-		this.localizacion = localizacion;
-		this.IDPer = IDPer;
-	}
-	
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getLocalizacion() {
-		return localizacion;
-	}
-	public void setLocalizacion(String localizacion) {
-		this.localizacion = localizacion;
-	}
-	public String getObjetivoO() {
-		return ObjetivoO;
-	}
-	public void setObjetivoO(String objetivoO) {
-		this.ObjetivoO = objetivoO;
-	}
-	public String getObjetivoL() {
-		return ObjetivoL;
-	}
-	public void setObjetivoL(String objetivoL) {
-		this.ObjetivoL = objetivoL;
-	}
+	public void inicializar() {
+		String linetxt = "";
+        String nuevalinea = "";
+        String nuevalinea2 = "";
+        String[] partes;
+        String[] aux;
+        int IDPer = 0;
+        int IDLug = 0;
+        int IDObj = 0;
+        int tipo = 0; //1->Lugar, 2->Personaje, 3->Objeto
 
-	@Override
-	public String toString() {
-		return "Personaje [nombre=" + nombre + ", localizacion=" + localizacion + ", ObjetivoO=" + ObjetivoO
-				+ ", ObjetivoL=" + ObjetivoL + ", IDPer=" + IDPer + "]";
+        try {
+            FileInputStream fichero = new FileInputStream("C:\\Users\\mhere\\OneDrive\\Escritorio\\U-TAD\\Curso2\\Programacion orientada a objetos\\08-POO\\Proyecto_Java\\Prueba_lectura.txt");
+            Scanner src = new Scanner(fichero);
+
+            while(src.hasNextLine()) {              
+                //System.out.println(linetxt);
+            	linetxt = src.nextLine(); 
+            	
+            	if(linetxt.contains("<")) {
+            		
+            		if(linetxt.contains("<Localizaciones>")) {
+            			tipo = 1;
+            		}
+            		else if(linetxt.contains("<Personajes>")) {
+            			tipo = 2;
+            		}
+            		else {
+            			tipo = 3;
+            		}
+            		
+            	}
+            	else {
+            		if(tipo == 1) {
+            			nuevalinea = linetxt.replace("(", "-").replace(")", "").replace(",","-").trim();
+                        partes = nuevalinea.split("-");
+                        nuevalinea2 = linetxt.replace(partes[0],"").replace("(", "").replace(")", "").replace(",","-").trim();
+                       // System.out.println("\n\n NUEVA LINEA 2 :"+nuevalinea2);
+                        aux = nuevalinea2.split("-");
+
+                        Lugar lugar = new Lugar(partes[0],aux,IDLug);
+                        lugares.add(lugar);
+                        IDLug ++;
+            		}
+            		else if(tipo == 2) {
+            			
+            			nuevalinea = linetxt.replace("(", "-").replace(")", "").replace(",","-").trim();
+                        partes = nuevalinea.split("-");
+                        nuevalinea2 = linetxt.replace(partes[0],"").replace("(", "").replace(")", "").replace(",","-").trim();
+                        //System.out.println("\n\n NUEVA LINEA 2 :"+nuevalinea2);
+                        aux = nuevalinea2.split("-");
+
+                        Personaje personaje = new Personaje(partes[0],aux[0],IDPer);
+                        personajes.add(personaje);
+                        IDPer ++;
+            		}
+            		else {
+            			nuevalinea = linetxt.replace("(", "-").replace(")", "").replace(",","-").trim();
+                        
+                        partes = nuevalinea.split("-");
+                        
+                        nuevalinea2 = linetxt.replace(partes[0],"").replace("(", "").replace(")", "").replace(",","-").trim();
+                        
+                        //System.out.println("\n\n NUEVA LINEA 2 :"+nuevalinea2);
+                        aux = nuevalinea2.split("-");
+
+                        Objeto objeto = new Objeto(partes[0],aux[0],IDObj);
+                        objetos.add(objeto);
+                        IDObj++;
+            		}
+            	}
+            }	
+            
+
+            src.close(); //Cerramos el scanner al fichero.
+            fichero.close(); //Cerramos el fichero.
+        } 
+        
+        catch (Exception e) { //Levanto una excepcion si no se ha podido leer el fichero
+            e.printStackTrace();
+        }
+    }
+	public void imprimir() {
+		System.out.println("[     LUGARES     ]\n");
+        
+        for(Lugar nini:lugares) {
+			System.out.print(nini.toString()+"\n");
+			System.out.println("");
+		}
+        System.out.println("[     PERSONAJES     ]\n");
+        for(Personaje lili:personajes) {
+			System.out.print(lili.toString()+"\n");
+			System.out.println("");
+		}
+        System.out.println("[     OBJETOS     ]\n");
+        for(Objeto claudio:objetos) {
+			System.out.print(claudio.toString()+"\n");
+			System.out.println("");
+		}
 	}
 	
-
-	
-	
+	public void objetivos(){
+		
+		String linetxt = "";
+        String nuevalinea = "";
+        String[] partes;
+		int tipo = 0;
+		int tamanio=0;
+		
+		try {
+			FileInputStream fichero = new FileInputStream("C:\\Users\\mhere\\OneDrive\\Escritorio\\U-TAD\\Curso2\\Programacion orientada a objetos\\08-POO\\Proyecto_Java\\Objetivos.txt");
+            Scanner src = new Scanner(fichero);
+            
+            while(src.hasNextLine()) {
+            	
+            	linetxt = src.nextLine();
+            	
+            	if(linetxt.contains("<")){	
+            		if(linetxt.contains("<Localizacion Personajes>")) {
+            			//System.out.println("hola");
+            			tipo = 1;
+            		}
+            		else if(linetxt.contains("<Posesion Objetos>")) {
+            			tipo = 2;
+            		}
+            		
+            	}
+            	else{
+            		nuevalinea = linetxt.replace("(", "-").replace(")", "").trim();
+            		partes = nuevalinea.split("-");
+            		tamanio = partes.length;
+            	
+            		
+            		if(tamanio < 2) {
+            			
+            		}
+            		else {
+            		if(tipo == 1){
+                        for(Personaje huevos:personajes){
+                        	if(huevos.getNombre().equals(partes[0])) {
+                        		huevos.setObjetivoL(partes[1]);
+                        	}
+                		}
+            		}
+            		
+            		else if(tipo == 2) {
+                        for(Personaje patata:personajes){
+                        	if(patata.getNombre().equals(partes[1])){
+                        		patata.setObjetivoO(partes[0]);
+                        		
+                        	}
+                		}
+            		}
+            	}
+            }
+        }
+            src.close(); //Cerramos el scanner al fichero.
+            fichero.close(); //Cerramos el fichero.
+		}
+        catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 }
