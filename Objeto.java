0f008553b453f95f25733ee9960ac659336f8b0b
Miguel <73239423+MiguelHGDC@users.noreply.github.com>
2020-12-27 13:36:22 +0100

    Update Objeto.java

--- Objeto.java
+++ Objeto.java
@@ -3,41 +3,33 @@ public class Objeto {
 
 		public String nombre;
 		public String localizacion;
+		int IDObj; //numero de identificacion del objeto
 
-		public Objeto(String nombre, String localizacion) {
+		public Objeto(String nombre, String localizacion, int IDObj) {
 			this.nombre = nombre;
 			this.localizacion = localizacion;
+			this.IDObj = IDObj;
 		}
-		
-		
-
+	
 		public String getNombre() {
 			return nombre;
 		}
 
-
-
 		public void setNombre(String nombre) {
 			this.nombre = nombre;
 		}
-
-
-
+		
 		public String getLocalizacion() {
 			return localizacion;
 		}
-
-
-
+		
 		public void setLocalizacion(String localizacion) {
 			this.localizacion = localizacion;
 		}
 
-
-
 		@Override
 		public String toString() {
-			return "Objeto [nombre: " + nombre + ", localizacion: " + localizacion + "  ]";
+			return "Objeto [nombre=" + nombre + ", localizacion=" + localizacion + ", IDObj=" + IDObj + "]";
 		}
 		
 }
