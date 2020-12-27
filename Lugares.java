5d7eefc526b046ded803a043e68429892ef0faa9
Miguel <73239423+MiguelHGDC@users.noreply.github.com>
2020-12-27 13:35:54 +0100

    Update Lugares.java

--- Lugares.java
+++ Lugares.java
@@ -3,11 +3,12 @@ import java.util.Arrays;
 public class Lugar {
 	public String nombre;
 	public String[] adyaciencias;
+	int IDLug; //numero de identificacion del lugar
 	
-	
-	public Lugar(String nombre, String[] adyaciencias) {
+	public Lugar(String nombre, String[] adyaciencias, int IDLug) {
 		this.nombre = nombre;
 		this.adyaciencias = adyaciencias;
+		this.IDLug = IDLug;
 	}
 	
 	
@@ -29,11 +30,14 @@ public class Lugar {
 	public String mostrar() {
 		return adyaciencias[1];
 	}
-	
+
+
 	@Override
 	public String toString() {
-		return "Lugar [nombre: " + nombre + ", adyaciencias: " + Arrays.toString(adyaciencias) + "  ]";
+		return "Lugar [nombre=" + nombre + ", adyaciencias=" + Arrays.toString(adyaciencias) + ", IDLug=" + IDLug + "]";
 	}
+	
+	
 
 
 	
